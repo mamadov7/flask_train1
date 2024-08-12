57b3932bb46ece85866509387403d9d7a6a7d980
mamadov7 <m.matin.kh8304@gmail.com>
2024-08-12 12:54:17 +0330

    rrender template for method get

--- base.py
+++ base.py
@@ -10,9 +10,20 @@ app=Flask(__name__)
 @app.route("/", method=['post','get'])
 def index():
     if request.method== 'get':
-        pass
+        return render_template(index)
     elif request.method=='post':
         pass
+
+
+
+
+
+
+
+
+
+if __name__=='__main__':
+    app.run(debug=True)
 
 
 
