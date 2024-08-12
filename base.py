968bf2920a872a463681c24386aa1b8d0852d47d
mamadov7 <m.matin.kh8304@gmail.com>
2024-08-12 14:28:12 +0330

    finish post method for route(/)

--- base.py
+++ base.py
@@ -4,19 +4,50 @@ from flask_sqlalchemy import SQLAlchemy
 
 app=Flask(__name__)
 
+####################data-base##################
+
+app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///user.db'
+app.config['SQLALCHEMY_POOL_RECYCLE'] = 280
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///yourdatabase.db?check_same_thread=False'
+
+db=SQLAlchemy(app)
 
 
+class user(db.Model):
+
+    id = db.Column(db.Integer, primary_key=True)
+    fname = db.Column(db.String(20))
+    lname = db.Column(db.String(20))
+    city = db.Column(db.String(20))
+    resume = db.Column(db.Text)
+
+
+with app.app_context():
+    db.create_all()
+
+
+
+
+
+
+##################app############################
 
 @app.route("/", methods=['POST','GET'])
 def index():
     if request.method== 'GET':
         return render_template('index.html')
     elif request.method=='POST':
-        name=request.form.get('first_name')
+        fname=request.form.get('first_name')
+        lname=request.form.get('last_name')
+        city=request.form.get('city')
+        resume=request.form.get('resume')
 
 
+        new_user=user(fname=fname , lname=lname , city=city , resume=resume)
 
-
+        db.session.add(new_user)
+        db.session.commit()
+        return render_template('index.html')
 
 
 
